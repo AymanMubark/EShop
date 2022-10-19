89dba3b41993d506c6df9e5f1ff5d38b8ee9cccd
Ayman Mubarak <cs.aymanmubark@gmail.com>
2022-10-19 09:42:38 +0300

    okay

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+        //private readonly DicountGrpcService _dicountGrpcService;
+        private readonly IMapper _mapper;
+        //private readonly IPublishEndpoint _publisher;
+
+        public BasketController(IBasketRepository repository,  IMapper mapper)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            //_dicountGrpcService = dicountGrpcService ?? throw new ArgumentNullException(nameof(dicountGrpcService));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            //_publisher = publisher ?? throw new ArgumentNullException(nameof(publisher));
+        }
+
+        [HttpGet("{userName}")]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart shoppingCart)
+        {
+            // TODO : Communication with Discount.Grpc
+            // and get latest price product into shopping cart
+            // cunsume discount grpc
+            foreach (var item in shoppingCart.Items)
+            {
+                //var coupon = await _dicountGrpcService.GetDiscount(item.ProductName);
+                //item.Price -= coupon.Amount;
+            }
+            var basket = await _repository.UpdateBasket(shoppingCart);
+            return Ok(basket ?? new ShoppingCart(shoppingCart.UserName));
+        }
+
+
+
+        [HttpDelete("{userName}")]
+        public async Task<ActionResult<ShoppingCart>> DeleteBasket(string userName)
+        {
+            await _repository.DeleteBasket(userName);
+            return Ok();
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
+        {
+            // Get exsiting basket with total price
+            // Create BasketCheckoutEvent -- Set TotalPrice on basketCheckout eventMessage
+            // Send checkout event to rqbbitmq
+            //remove  the basekt
+
+            // Get exsiting basket with total price
+            var basket = await _repository.GetBasket(basketCheckout.UserName);
+            if (basket == null)
+            {
+                return BadRequest();
+            }
+
+            // Send checkout event to rqbbitmq
+            //var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
+            //eventMessage.TotalPrice = basket.TotalPrice;
+            //await _publisher.Publish(eventMessage);
+
+            await _repository.DeleteBasket(basketCheckout.UserName);
+
+            return Accepted();
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCartItem
+    {
+        public string ProductId { get; set; } = "";
+        public string ProductName { get; set; } = "";
+        public string ProductImageUrl { get; set; } = "";
+        public int Quantity { get; set; } = 1;
+        public decimal Price { get; set; } = 0;
+    }
+}
